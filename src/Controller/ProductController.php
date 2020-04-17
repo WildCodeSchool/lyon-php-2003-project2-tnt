891c03e4c04b7cd54369bd2e7b3f04c422d721d8
Justine <pignet.justine@gmail.com>
2020-04-17 10:28:56 +0200

    ajout d'un formulaire pour l'ajout de biens, minimum de style

---
+++ src/Controller/ProductController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Controller;
+
+use App\Model\ProductManager;
+
+/**
+ * Class ProductController
+ *
+ */
+class ProductController extends AbstractController
+{
+    /**
+     * Display item creation page
+     *
+     * @return string
+     * @throws \Twig\Error\LoaderError
+     * @throws \Twig\Error\RuntimeError
+     * @throws \Twig\Error\SyntaxError
+     */
+    public function add_good()
+    {
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $productManager = new ProductManager();
+            $product = [
+                'title' => $_POST['title'],
+            ];
+            $id = $productManager->insert($product);
+            header('Location:/product/show/' . $id);
+        }
+        return $this->twig->render('Product/addGood.html.twig');
+    }
+}
---
+++ src/Model/ProductManager.php
@@ -0,0 +1,19 @@
+<?php
+
+
+/**
+ * Created by PhpStorm.
+ * User: sylvain
+ * Date: 07/03/18
+ * Time: 18:20
+ * PHP version 7
+ */
+
+namespace App\Model;
+
+/**
+ *
+ */
+class ProductManager extends AbstractManager {
+
+}
