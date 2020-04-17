73130210c5b820e402ec5aad5cb17cfbf03347d0
Justine <pignet.justine@gmail.com>
2020-04-17 11:14:20 +0200

    formulaire service, min style

---
+++ src/Controller/ProductController.php
@@ -0,0 +1,34 @@
+<?php
+
+
+namespace App\Controller;
+
+use App\Model\ProductManager;
+
+/**
+ * Class ProductController
+ *
+ */
+class ProductController extends AbstractController
+{
+    /**
+     * Display item creation page
+     *
+     * @return string
+     * @throws \Twig\Error\LoaderError
+     * @throws \Twig\Error\RuntimeError
+     * @throws \Twig\Error\SyntaxError
+     */
+    public function addService()
+    {
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $productManager = new ProductManager();
+            $product = [
+                'title' => $_POST['title'],
+            ];
+            $id = $productManager->insert($product);
+            header('Location:/product/show/' . $id);
+        }
+        return $this->twig->render('Product/addService.html.twig');
+    }
+}
