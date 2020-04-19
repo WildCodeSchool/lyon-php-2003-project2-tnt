c7895541d11a8d958b51e3bd13363b8c6d4f8844
Brice d'Arménia <brice-d-armenia_student2020@wilder.school>
2020-04-19 16:45:59 +0200

    Update ProductController.php

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -4,10 +4,6 @@ namespace App\Controller;
 
 use App\Model\ProductManager;
 
-/**
- * Class ProductController
- *
- */
 class ProductController extends AbstractController
 {
     /**
@@ -22,7 +18,28 @@ class ProductController extends AbstractController
 
         return $this->twig->render('Product/listProduct.html.twig', ['products'=>$products]);
     }
-    
+
+    /**
+     * Display item creation page
+     *
+     * @return string
+     * @throws \Twig\Error\LoaderError
+     * @throws \Twig\Error\RuntimeError
+     * @throws \Twig\Error\SyntaxError
+     */
+    public function addService()
+    {
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $productManager = new ProductManager();
+            $product = [
+                'title' => $_POST['title'],
+            ];
+            $id = $productManager->insert($product);
+            header('Location:/product/show/' . $id);
+        }
+        return $this->twig->render('Product/addService.html.twig');
+    }
+
     /**
      * Display item creation page
      *
@@ -42,17 +59,4 @@ class ProductController extends AbstractController
             header('Location:/product/show/' . $id);
         }
         return $this->twig->render('Product/addGood.html.twig');
-      
-    /**
-     * Affiche la liste des produits
-     *
-     */
-    public function listProduct()
-    {
-        $productManager = new ProductManager();
-        $products = $productManager->selectAll();
-
-        return $this->twig->render('Product/listProduct.html.twig', ['products'=>$products]);
-
-    }
 }
