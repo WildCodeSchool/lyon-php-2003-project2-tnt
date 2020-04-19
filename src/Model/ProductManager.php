6ca213a3d690cf7b5d798e53ec228474758d57af
Brice d'Arménia <brice-d-armenia_student2020@wilder.school>
2020-04-19 16:49:26 +0200

    Update ProductManager.php

--- src/Model/ProductManager.php
+++ src/Model/ProductManager.php
@@ -1,20 +1,35 @@
 <?php
 
+
 namespace App\Model;
 
 class ProductManager extends AbstractManager
 {
+
     /**
      *
      */
     const TABLE = 'product';
-
     /**
      *  Initializes this class.
      */
     public function __construct()
     {
         parent::__construct(self::TABLE);
+    }
+
+    /**
+     * Recupère tout depuis les tables product / .
+     *
+     * @return array
+     */
+    public function selectAll(): array
+    {
+        return $this->pdo->query('SELECT * FROM ' . $this->table .
+                                 ' JOIN user ON user.id = product.user_id 
+                                 JOIN product_type ON product_type.id = product.product_type_id
+                                 JOIN exchange_type ON exchange_type.id =product.exchange_type_id
+                                 HAVING product_type_id="1"')->fetchAll();
     }
 
     /**
@@ -57,18 +72,5 @@ class ProductManager extends AbstractManager
         $statement->bindValue('title', $product['title'], \PDO::PARAM_STR);
 
         return $statement->execute();
-      
-    /**
-     * Recupère tout depuis les tables product / .
-     *
-     * @return array
-     */
-    public function selectAll(): array
-    {
-        return $this->pdo->query('SELECT * FROM ' . $this->table .
-                                 ' JOIN user ON user.id = product.user_id 
-                                 JOIN product_type ON product_type.id = product.product_type_id
-                                 JOIN exchange_type ON exchange_type.id =product.exchange_type_id
-                                 HAVING product_type_id="1"')->fetchAll();
     }
 }
