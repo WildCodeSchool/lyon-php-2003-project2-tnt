f1848396ae56ed957b4a44bb4a1c1c81d743f755
Brice d'Arménia <brice-d-armenia_student2020@wilder.school>
2020-04-19 16:40:20 +0200

    Update ProductController.php

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -39,16 +39,4 @@ class ProductController extends AbstractController
         }
         return $this->twig->render('Product/addService.html.twig');
     }
-  
-    /**
-     * Affiche la liste des produits
-     *
-     */
-    public function listProduct()
-    {
-        $productManager = new ProductManager();
-        $products = $productManager->selectAll();
-
-        return $this->twig->render('Product/listProduct.html.twig', ['products'=>$products]);
-    }
 }
