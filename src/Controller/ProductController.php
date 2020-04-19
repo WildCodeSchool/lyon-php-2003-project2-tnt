3d2ca150a8c85ba3a57b352c80ca55f8d1608df6
Brice d'Arménia <brice-d-armenia_student2020@wilder.school>
2020-04-19 16:33:13 +0200

    Update ProductController.php

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -1,13 +1,25 @@
 <?php
-
+<?php
 
 namespace App\Controller;
 
 use App\Model\ProductManager;
 
-
 class ProductController extends AbstractController
 {
+    /**
+     * Affiche la liste des produits
+     *
+     */
+
+    public function listProduct()
+    {
+        $productManager = new ProductManager();
+        $products = $productManager->selectAll();
+
+        return $this->twig->render('Product/listProduct.html.twig', ['products'=>$products]);
+    }
+
     /**
      * Display item creation page
      *
@@ -39,4 +51,5 @@ class ProductController extends AbstractController
         $products = $productManager->selectAll();
 
         return $this->twig->render('Product/listProduct.html.twig', ['products'=>$products]);
+    }
 }
