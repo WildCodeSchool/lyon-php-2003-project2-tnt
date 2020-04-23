521ca92a40ab11c89321d66ed2d0fbd60bd407db
n.gaget69@gmail.com <n.gaget69@gmail.com>
2020-04-23 09:16:32 +0200

    validation form

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -64,12 +64,57 @@ class ProductController extends AbstractController
     public function addGood()
     {
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-            $productManager = new ProductManager();
-            $product = [
-                'title' => $_POST['title'],
-            ];
-            $id = $productManager->insert($product);
-            header('Location:/product/show/' . $id);
+            $title = $category = $subCategory = $etat = $description = $don = $echange = $proposition = '';
+            $errors =[];
+            if (!empty($_POST['title'])) {
+                $title = AbstractController::testInput($_POST['title']);
+            } else {
+                $errors['title'] = "Que souhaitez vous proposer?";
+            }
+            if (!empty($_POST['category'])) {
+                $category = AbstractController::testInput($_POST['category']);
+            } else {
+                $errors['category'] = "Renseignez une catégorie";
+            }
+            if (!empty($_POST['subCategory'])) {
+                $subCategory = AbstractController::testInput($_POST['subCategory']);
+            }
+            if (!empty($_POST['etat'])) {
+                $etat = AbstractController::testInput($_POST['etat']);
+            } else {
+                $errors['etat'] = "Précisez l'état";
+            }
+            if (!empty($_POST['description'])) {
+                $description = AbstractController::testInput($_POST['description']);
+            } else {
+                $errors['descritpion'] = "Renseignez d'avantage d'information";
+            }
+
+            if (!empty($_POST['don'])) {
+                $don = $_POST['don'];
+            }
+            if (!empty($_POST['echange'])) {
+                $echange = AbstractController::testInput($_POST['proposition']);
+            }
+            if (!empty($_POST['proposition'])) {
+                $proposition = AbstractController::testInput($_POST['proposition']);
+            }
+            if (empty($errors)) {
+                $productManager = new ProductManager();
+                $product=[
+                    'title' => $title,
+                    'category' => $category,
+                    'subCategory' => $subCategory,
+                    'etat' => $etat,
+                    'description' => $description,
+                    'don' => $don,
+                    'echange' => $echange,
+                    'proposition' => $proposition
+                ];
+            }
+
+            $id = $productManager->insertProduct($product);
+            header('Location:/product/advertGood/' . $id);
         }
         return $this->twig->render('Product/addGood.html.twig');
     }
