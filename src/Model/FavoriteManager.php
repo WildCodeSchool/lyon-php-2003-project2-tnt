0e944d83f7894b6ed379892a93d90df34c166ff8
raphael-morales <raphael.morales@hotmail.fr>
2020-05-12 18:27:10 +0200

    création fichier favoriteManager, méthode addFavorite

---
+++ src/Model/FavoriteManager.php
@@ -0,0 +1,22 @@
+<?php
+
+
+namespace App\Model;
+
+use PDO;
+
+class FavoriteManager extends AbstractManager{
+
+    const TABLE = 'Favorite';
+
+
+    public function addFavorite(array $favorite){
+
+        $query = "INSERT INTO " . $this->table . " (product_id, user_id) VALUES (:product_id, :user_id)";
+        $statement = $this->pdo->prepare($query);
+        $statement->bindValue('product_id', $favorite['product_id'], \PDO::PARAM_INT);
+        $statement->bindValue('user_id', $favorite['user_id'], \PDO::PARAM_INT);
+        return $statement->execute();
+    }
+
+}
